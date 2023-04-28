6066fe725fc99d44a812cfe8c309113dc9b77dcf
Jared Kozar <jared.rkozar@gmail.com>
2023-04-27 20:01:13 -0400

    added save note button

--- public/client.js
+++ public/client.js
@@ -1,5 +1,6 @@
 let noNoteView = document.getElementsByClassName("noNoteSelected")[0]
 let noteTextView = document.getElementsByClassName("noteFields")[0]
+let saveNoteButton = document.getElementById("saveNoteButton")
 
 function getNotes() {
     const arrayStrings = window.location.href.split("/");
@@ -7,12 +8,14 @@ function getNotes() {
 }
 
 function newNote() {
-    noNoteView.style.display = "none"
     noteTextView.style.display = "block"
+    noNoteView.style.display = "none"
+    saveNoteButton.style.display = "block"
 }
 
 window.onload = () => {
     noNoteView.style.display = "block"
     noteTextView.style.display = "none"
+    saveNoteButton.style.display = "none"
     getNotes();
 };
